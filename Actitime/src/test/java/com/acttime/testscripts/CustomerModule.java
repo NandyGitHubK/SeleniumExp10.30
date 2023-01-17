52de00e5bbc61a88b124bd5379469b85e7b337f3
anubhavadesign <anubhavadesign@localhost>
2023-01-17 10:47:48 +0530

    First commit.

---
+++ Actitime/src/test/java/com/acttime/testscripts/CustomerModule.java
@@ -0,0 +1,14 @@
+package com.acttime.testscripts;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+	@Test
+	public void createCustomer()
+	{
+		Reporter.log("Hi",true);
+	}
+
+}
